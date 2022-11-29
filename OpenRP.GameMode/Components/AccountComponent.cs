5f9d03b78c2dd2924b4d9494049c2a5db8c26a70
Koert Lichtendonk <C.Lichtendonk@live.nl>
2022-11-29 22:04:53 +0100

    Component and system test

---
+++ OpenRP.GameMode/Components/AccountComponent.cs
@@ -0,0 +1,13 @@
+﻿using OpenRP.GameMode.Data.Models;
+using SampSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenRP.GameMode.Components
+{
+    public class AccountComponent : Component
+    {
+        public Account Account { get; set; }
+    }
+}
