1e9e69872c52a0db097b793b1eb613fcd8190915
Koert Lichtendonk <C.Lichtendonk@live.nl>
2023-12-03 15:49:54 +0100

    CreateInventory

---
+++ OpenRP.GameMode/Features/Inventories/Helpers/InventoryHelper.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using OpenRP.GameMode.Configuration;
+using OpenRP.GameMode.Data;
+using OpenRP.GameMode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenRP.GameMode.Features.Inventories.Helpers
+{
+    public static class InventoryHelper
+    {
+        public static ulong? CreateInventory(string inventoryName, uint? maxWeightInGrams)
+        {
+            try
+            {
+                using (var context = new DataContext())
+                {
+                    Inventory newInventory = new Inventory() { Name =  inventoryName, MaxWeight = maxWeightInGrams };
+                    context.Inventories.Add(newInventory);
+                    context.SaveChanges();
+
+                    return newInventory.Id;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return null;
+        }
+    }
+}
