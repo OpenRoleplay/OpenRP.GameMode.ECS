d6de2fe15d024d7bcf01de08d3a642cc9a901a4b
Koert Lichtendonk <C.Lichtendonk@live.nl>
2023-12-03 14:46:39 +0100

    /setskin through EF Core + EF Core dependency fix

---
+++ OpenRP.GameMode/Migrations/20231129182540_Update.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenRP.GameMode.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Skin",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 26);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Skin",
+                table: "Characters");
+        }
+    }
+}
--- OpenRP.GameMode/Program.cs
+++ OpenRP.GameMode/Program.cs
@@ -1,5 +1,9 @@
 ﻿using SampSharp.Core;
 using SampSharp.Entities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace OpenRP.GameMode
 {
@@ -7,9 +11,44 @@ namespace OpenRP.GameMode
     {
         static void Main(string[] args)
         {
+            var appDirPath = GetApplicationLocation();
+
+            ApplyAssemblyResolveFix(appDirPath);
+
             new GameModeBuilder()
                 .UseEcs<Startup>()
                 .Run();
+        }
+
+        private static string GetApplicationLocation()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Unable to determine the entry assembly.");
+            var appDirPath = Path.GetDirectoryName(entryAssembly.Location);
+            if (string.IsNullOrEmpty(appDirPath))
+            {
+                throw new InvalidOperationException("Unable to determine the application's location.");
+            }
+            return appDirPath;
+        }
+
+        private static void ApplyAssemblyResolveFix(string searchDirectory)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (object? sender, ResolveEventArgs args) =>
+            {
+                Debug.WriteLine($"Resolving assembly: {args.Name}");
+
+                var assemblyName = new AssemblyName(args.Name).Name;
+                var resolvedPath = Path.Combine(searchDirectory, $"{assemblyName}.dll");
+
+                if (File.Exists(resolvedPath))
+                {
+                    Debug.WriteLine($"Resolved path: {resolvedPath}");
+                    return Assembly.LoadFile(resolvedPath);
+                }
+
+                Debug.WriteLine("Not resolved.");
+                return null;
+            };
         }
     }
 }
