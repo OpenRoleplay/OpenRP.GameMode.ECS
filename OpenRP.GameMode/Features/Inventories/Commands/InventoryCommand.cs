cc87c5eb78da8f8389de066656ae480343f46b45
Koert Lichtendonk <contact@koertlichtendonk.nl>
2023-12-03 17:47:49 +0100

    /inventory command

---
+++ OpenRP.GameMode/Features/Inventories/Commands/InventoryCommand.cs
@@ -0,0 +1,24 @@
+﻿using OpenRP.GameMode.Data.Models;
+using OpenRP.GameMode.Data;
+using OpenRP.GameMode.Features.Accounts.Components;
+using SampSharp.Entities.SAMP.Commands;
+using SampSharp.Entities.SAMP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenRP.GameMode.Features.Characters.Helpers;
+using SampSharp.Entities;
+
+namespace OpenRP.GameMode.Features.Inventories.Commands
+{
+    public class InventoryCommand : ISystem
+    {
+        [PlayerCommand]
+        public void Inventory(Player player)
+        {
+            CharacterComponent characterComponent = player.GetComponent<CharacterComponent>();
+
+            characterComponent.CharacterPlayingAs.GetCharacterInventory();
+        }
+    }
+}
