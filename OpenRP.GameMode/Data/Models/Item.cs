413743145946bd3161f506de48d2c142f57530ee
Koert Lichtendonk <koert@adjust-it.nl>
2023-12-04 12:07:49 +0100

    Item Types

--- OpenRP.GameMode/Data/Models/Item.cs
+++ OpenRP.GameMode/Data/Models/Item.cs
@@ -9,6 +9,7 @@ namespace OpenRP.GameMode.Data.Models
         public ulong Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public ItemType Type { get; set; }
         public uint Weight { get; set; }
         public uint MaxUses { get; set; }
         public bool KeepOnDeath { get; set; }
