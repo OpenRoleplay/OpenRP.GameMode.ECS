fbfe3d6e945385b324337887cf6df1ec30495a6a
Koert Lichtendonk <C.Lichtendonk@live.nl>
2023-12-10 20:24:48 +0100

    WIP Chat to be tested

--- OpenRP.GameMode/Features/Chat/Helpers/ChatHelper.cs
+++ OpenRP.GameMode/Features/Chat/Helpers/ChatHelper.cs
@@ -5,6 +5,8 @@ using OpenRP.GameMode.Features.Chat.Enums;
 using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
 using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace OpenRP.GameMode.Features.Chat.Helpers
 {
@@ -73,7 +75,7 @@ namespace OpenRP.GameMode.Features.Chat.Helpers
                         player.SetChatBubble(CHAT_ACTION_AME, Color.White, 20, CHAT_ACTION_AME.Length * 60);
                         break;
                     case PlayerChatMessageType.TALK:
-                        //player.SendTalkMessage(text);
+                        player.SendTalkMessage(entityManager, text);
                         break;
                 }
             }
@@ -88,6 +90,99 @@ namespace OpenRP.GameMode.Features.Chat.Helpers
                     foreachPlayer.SendClientMessage(text);
                 }
             }
+        }
+
+
+        public static void SendTalkMessage(this Player player, IEntityManager entityManager, string text)
+        {
+            CharacterComponent characterComponent = player.GetComponent<CharacterComponent>();
+
+            foreach (Player foreachPlayer in entityManager.GetComponents<Player>())
+            {
+                string new_text = text;
+
+                float distance = foreachPlayer.GetDistanceFromPoint(player.Position);
+
+                if (distance <= 5.0 && foreachPlayer.VirtualWorld == player.VirtualWorld)
+                {
+                    Color min_color = Color.FromString("F5F5F5", ColorFormat.RGB);
+                    Color max_color = Color.FromString("C0C0C0", ColorFormat.RGB);
+                    Color result_color;
+
+                    if (distance >= 3.0)
+                    {
+                        Random change_words_perc = new Random();
+                        if (change_words_perc.Next(0, 100) <= 5)
+                        {
+                            new_text = Regex.Replace(new_text, "([Ff])at(?!tie)", "$1attie");
+                            new_text = Regex.Replace(new_text, "[Gg]reat(?! [pP]olzin(?:i)?)", "Great Polzini");
+                            new_text = Regex.Replace(new_text, "([Kk])im ([Jj])ong ([Uu])n", "$1ir $2ong $3n");
+                            new_text = Regex.Replace(new_text, "([Kk])im", "$1ir");
+                        }
+
+                        new_text = new_text.Muffle(distance, 3, 5);
+
+                        //float distance_in_perc = (100 / 2) * distance;
+
+                        //double resultRed = min_color.R + distance_in_perc * (max_color.R - min_color.R);
+                        //double resultGreen = min_color.G + distance_in_perc * (max_color.G - min_color.G);
+                        //double resultBlue = min_color.B + distance_in_perc * (max_color.B - min_color.B);
+
+                        //result_color = new Color(Convert.ToByte(resultRed), Convert.ToByte(resultGreen), Convert.ToByte(resultBlue));
+
+                        result_color = min_color;
+                    }
+                    else
+                    {
+                        result_color = min_color;
+                    }
+
+                    StringBuilder sb = new StringBuilder();
+
+                    sb.Append(String.Format("{0}{1} ", result_color.ToString(), characterComponent.CharacterPlayingAs.GetCharacterName()));
+
+                    if (!String.IsNullOrEmpty(characterComponent.CharacterPlayingAs.Accent))
+                    {
+                        sb.Append(String.Format("[{0} accent] ", characterComponent.CharacterPlayingAs.Accent));
+                    }
+
+                    sb.Append(String.Format("says: {0}", new_text));
+
+                    player.SendClientMessage(sb.ToString());
+                }
+            }
+        }
+
+
+        public static string Muffle(this string text, float percentage)
+        {
+            char[] char_array = text.ToCharArray();
+
+            Random random_percentage = new Random();
+
+            for (int i = 0; i < char_array.Length; i++)
+            {
+                if (char_array[i] != ' ')
+                {
+                    if (random_percentage.Next(0, 100) <= percentage)
+                    {
+                        char_array[i] = '.';
+                    }
+                }
+            }
+
+            return new string(char_array);
+        }
+
+        public static string Muffle(this string text, float player_distance, float acceptable_distance = 3.0f, float max_distance = 5.0f)
+        {
+            if (player_distance >= acceptable_distance)
+            {
+                float percentage = (30 / (max_distance - acceptable_distance) * (player_distance - acceptable_distance));
+
+                return text.Muffle(percentage);
+            }
+            return text;
         }
     }
 }
---
+++ OpenRP.GameMode/Features/Chat/Systems/ChatSystem.cs
@@ -0,0 +1,20 @@
+﻿using OpenRP.GameMode.Features.Chat.Helpers;
+using SampSharp.Entities;
+using SampSharp.Entities.SAMP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenRP.GameMode.Features.Chat.Systems
+{
+    public class ChatSystem : ISystem
+    {
+        [Event]
+        public bool OnPlayerText(Player player, string text, IEntityManager entityManager)
+        {
+            player.SendTalkMessage(entityManager, text);
+
+            return false;
+        }
+    }
+}
